fa880cc1bb429ee8075979b23592a8b6dac5b4ab
DAVIT.CHOEUN <DAVIT.CHOEUN@PNC.LAN>
2024-05-13 17:35:57 +0700

    homework vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <NameForm />
+</template>
+
+<script>
+import NameForm from './components/NameForm.vue';
+export default{
+  name:"App",
+    components:{
+      NameForm
+    }
+
+}
+</script>
+
+<style></style>
